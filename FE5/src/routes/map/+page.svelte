832ae6b9e025ddbf12d2625b4ebca62784d7444e
Shubham Patel <66919503+shubhp03@users.noreply.github.com>
2024-04-18 14:36:12 -0500

    Updated Map Page!
    This is the latest (and almost completed) map page!

--- FE5/src/routes/map/+page.svelte
+++ FE5/src/routes/map/+page.svelte
@@ -4,23 +4,22 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1tK70OEmtrTxAPkUDnOohMxgzCzz8Z90&ehbc=2E312F&noprof=1" width="640" height="480" class="welcome"></iframe>
+	
+	<h1>Find your events on a Map!</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		This goes beyond just what you can find on our <a href="https://calendar.utdallas.edu">Comet Calendar</a> website. Here, any organization or group can push their events, for all
+		the students to find!
 	</p>
 
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
 </div>
+
+<style>
+	.welcome {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+	}
+</style>
